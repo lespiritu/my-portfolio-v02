318f16369b8add14363adc8c47b83bb9856e9a40
Zeke <zeke@edamama.ph>
2022-08-11 23:45:01 +0800

    added some changes

--- apps/frontend/src/app/pages/home.tsx
+++ apps/frontend/src/app/pages/home.tsx
@@ -1,6 +1,21 @@
 import { FC } from 'react';
 import { Navigation } from '../components';
+import { Typography } from 'ui';
 
 export const Home: FC = () => {
-  return <Navigation />;
+  return (
+    <>
+      <Navigation />;
+      <hr />
+      <Typography variant="header1">Header 1</Typography>
+      <Typography variant="header2">Header 2</Typography>
+      <Typography variant="header3">Header 3</Typography>
+      <Typography variant="header4">Header 4</Typography>
+      <Typography variant="header5">Header 5</Typography>
+      <Typography variant="paragraph">Paragraph</Typography>
+      <Typography variant="button">button</Typography>
+      <Typography variant="input">input</Typography>
+      <Typography variant="label">label</Typography>
+    </>
+  );
 };
